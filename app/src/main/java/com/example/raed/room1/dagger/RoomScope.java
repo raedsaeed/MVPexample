6c8d901cb74174d96f280b933f9ca075a5fe3049
raedsaeed <raedsaeed46@hotmail.com>
2018-02-15 08:05:41 +0200

    Add RoomScope class

---
+++ app/src/main/java/com/example/raed/room1/dagger/RoomScope.java
@@ -0,0 +1,15 @@
+package com.example.raed.room1.dagger;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
+import javax.inject.Scope;
+
+/**
+ * Created by raed on 2/14/18.
+ */
+
+@Scope
+@Retention(RetentionPolicy.CLASS)
+public @interface RoomScope {
+}
